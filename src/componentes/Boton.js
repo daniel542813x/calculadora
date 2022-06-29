50929694d6a7657ba2ef236a691047595bcd7485
Daniel Estevan Madroñero Moreno <Daniel Estevan Madroero Moreno>
2022-06-29 14:57:11 -0500

    Calculadora completada

---
+++ src/componentes/Boton.js
@@ -0,0 +1,14 @@
+import '../css/Boton.css'
+import React from 'react'
+function Boton(props){
+    const esOperador=valor=>{
+        return isNaN(valor) && (valor != '.') && (valor!='=');
+    }
+    return (
+        <div className={`boton-contenedor ${esOperador(props.children)? 'operador': ''}`.trimEnd()}
+            onClick={()=>props.manejarClic(props.children)}>
+            {props.children}
+        </div>
+    )
+}
+export default Boton;
